2935edb0e8c646dd19e6899c5e5a4d532eb41fb7
Akin Aguda <lordtitan7@gmail.com>
2019-03-28 01:20:08 +0100

    added prop types, fixed babel problem, written some question code

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,12 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Cl from './index.module.css';
+import './index.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit
-            {' '}
-            <code>src/App.js</code>
-            {' '}
-and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className={Cl.top} />
     );
   }
 }
