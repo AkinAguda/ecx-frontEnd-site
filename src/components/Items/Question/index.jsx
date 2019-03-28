fc971e7a801411eb24929d994d6bbff93a0dbe9d
Okafor Irene <okaforirenen@gmail.com>
2019-03-28 15:44:36 +0100

    removed some unnecassary divs and changed them to HOCs

--- src/components/Items/Question/index.jsx
+++ src/components/Items/Question/index.jsx
@@ -2,21 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Cl from './Question.module.css';
 import Option from '../Option';
+import Wrapper from '../../HOCs/Wrapper';
 
 const Question = (props) => {
   const { questions } = props;
   return (
     <div className={Cl.container}>
-      {questions.map(({ type, option, question }) => (
-        <div key={option}>
-          <h1 className={Cl.question}>{question}</h1>
-          {
+      <Wrapper>
+        {
+         questions.map(({ type, option, question }) => (
+           <Wrapper key={option}>
+             <h1 className={Cl.question}>{question}</h1>
+             {
                 option.map(opt => (
                   <Option option={opt} type={type} key={opt} className={Cl.option} />
                 ))
-            }
-        </div>
-      ))}
+             }
+           </Wrapper>
+         ))}
+      </Wrapper>
     </div>
   );
 };
