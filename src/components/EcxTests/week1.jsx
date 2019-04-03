f033eec88afb3126a165796167322d5811adfd14
Akin Aguda <lordtitan7@gmail.com>
2019-04-03 02:30:51 +0100

    abstracted week logic

--- src/components/EcxTests/week1.jsx
+++ src/components/EcxTests/week1.jsx
@@ -1,47 +1,7 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import Header from '../Items/Nav';
-import SideDrawer from '../Items/SideDrawer';
-import Question from '../Items/Question';
-import Wrapper from '../HOCs/Wrapper';
+import React from 'react';
 import { week1 } from '../../utils';
+import Week from '../Items/Week';
 
-class weekOne extends Component {
-  constructor(props) {
-    super(props);
-    const { isOpen } = this.props;
-    this.state = {
-      // eslint-disable-next-line react/no-unused-state
-      isOpen,
-    };
-  }
+const weekOne = () => <Week week={week1} />;
 
-  render() {
-    const { openNav, closeNav, isOpen } = this.props;
-    return (
-      <Wrapper>
-        <Header open={openNav} close={closeNav} />
-        <SideDrawer isOpen={isOpen} close={closeNav} />
-        <Question questions={week1} />
-      </Wrapper>
-    );
-  }
-}
-
-const mapStateToProps = state => ({
-  isOpen: state.isOpen,
-});
-
-const mapDispatchToProps = dispatch => ({
-  openNav: () => dispatch({ type: 'OPEN SIDE DRAWER' }),
-  closeNav: () => dispatch({ type: 'CLOSE SIDE DRAWER' }),
-});
-
-weekOne.propTypes = {
-  openNav: PropTypes.string.isRequired,
-  closeNav: PropTypes.string.isRequired,
-  isOpen: PropTypes.string.isRequired,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(weekOne);
+export default weekOne;
--- src/components/EcxTests/week2.jsx
+++ src/components/EcxTests/week2.jsx
@@ -1,47 +1,7 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import Header from '../Items/Nav';
-import SideDrawer from '../Items/SideDrawer';
-import Question from '../Items/Question';
-import Wrapper from '../HOCs/Wrapper';
+import React from 'react';
 import { week2 } from '../../utils';
+import Week from '../Items/Week';
 
-class weekTwo extends Component {
-  constructor(props) {
-    super(props);
-    const { isOpen } = this.props;
-    this.state = {
-      // eslint-disable-next-line react/no-unused-state
-      isOpen,
-    };
-  }
+const weekOne = () => <Week week={week2} />;
 
-  render() {
-    const { openNav, closeNav, isOpen } = this.props;
-    return (
-      <Wrapper>
-        <Header open={openNav} close={closeNav} />
-        <SideDrawer isOpen={isOpen} close={closeNav} />
-        <Question questions={week2} />
-      </Wrapper>
-    );
-  }
-}
-
-const mapStateToProps = state => ({
-  isOpen: state.isOpen,
-});
-
-const mapDispatchToProps = dispatch => ({
-  openNav: () => dispatch({ type: 'OPEN SIDE DRAWER' }),
-  closeNav: () => dispatch({ type: 'CLOSE SIDE DRAWER' }),
-});
-
-weekTwo.propTypes = {
-  openNav: PropTypes.string.isRequired,
-  closeNav: PropTypes.string.isRequired,
-  isOpen: PropTypes.string.isRequired,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(weekTwo);
+export default weekOne;
