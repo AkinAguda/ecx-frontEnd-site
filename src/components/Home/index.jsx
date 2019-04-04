ee701dd334d3cb9f0b38b42917689b239610ee1b
Akin Aguda <lordtitan7@gmail.com>
2019-04-05 00:25:45 +0100

    started work on home

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
-import { Link } from 'react-router-dom';
-import Wrapper from '../HOCs/Wrapper';
+import { NavLink } from 'react-router-dom';
+// import Wrapper from '../HOCs/Wrapper';
 import classes from './index.module.css';
 
 const Home = () => (
-  <Wrapper>
-    <div className={classes.Container}>
+  <div className={classes.body}>
+    {/* <div className={classes.Container}>
       <h1>Home</h1>
     </div>
     <div className={classes.Body}>
@@ -14,12 +14,26 @@ const Home = () => (
         <br />This is just a way to revise what
         you have learnt so far.
       </p>
-      <Link to="/week-one">Week1</Link>
+      <NavLink to="/week-one">Week1</NavLink>
       <br />
-      <Link to="/week-two">Week2</Link>
+      <NavLink to="/week-two">Week2</NavLink>
+    </div> */}
+    <div className={classes.Body}>
+      <p>
+      Welcome to the quiz website for the ECX Front End Track.
+        <br />This is just a way to revise what
+        you have learnt so far.
+      </p>
     </div>
-  </Wrapper>
-
+    <div className={classes.base}>
+      <div className={classes.tests}>
+        <NavLink to="/week-one">Week1</NavLink>
+        <br />
+        <NavLink to="/week-two">Week2</NavLink>
+      </div>
+    </div>
+    <footer>Built by Irene and Akin</footer>
+  </div>
 );
 
 export default Home;
