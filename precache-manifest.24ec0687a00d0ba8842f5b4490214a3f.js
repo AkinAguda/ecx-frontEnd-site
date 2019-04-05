2287a8be0aa63ffdf00a96ad7000b042fae337be
Akin Aguda <lordtitan7@gmail.com>
2019-04-05 14:13:57 +0100

    Updates

--- precache-manifest.24ec0687a00d0ba8842f5b4490214a3f.js
+++ precache-manifest.24ec0687a00d0ba8842f5b4490214a3f.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/AkinAguda/ecx-frontEnd-site/static/js/runtime~main.0b7a5ad2.js"
   },
   {
-    "revision": "47a9a3254f6f3ab4130f",
-    "url": "/AkinAguda/ecx-frontEnd-site/static/js/main.0e73f7f2.chunk.js"
+    "revision": "b9237fc1bd69577b13c5",
+    "url": "/AkinAguda/ecx-frontEnd-site/static/js/main.ab2f69d1.chunk.js"
   },
   {
     "revision": "3f69d65be8a2202f1364",
     "url": "/AkinAguda/ecx-frontEnd-site/static/js/2.29033cd2.chunk.js"
   },
   {
-    "revision": "47a9a3254f6f3ab4130f",
+    "revision": "b9237fc1bd69577b13c5",
     "url": "/AkinAguda/ecx-frontEnd-site/static/css/main.362665af.chunk.css"
   },
   {
-    "revision": "e0bbd60decc4c9d43fdba8caf8bef1da",
+    "revision": "64d656f0bbfd8e515013b86fa1db0919",
     "url": "/AkinAguda/ecx-frontEnd-site/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/AkinAguda/ecx-frontEnd-site/precache-manifest.4e5fe14e1acc374129a57d00611b03f5.js"
+  "/AkinAguda/ecx-frontEnd-site/precache-manifest.24ec0687a00d0ba8842f5b4490214a3f.js"
 );
 
 workbox.clientsClaim();
