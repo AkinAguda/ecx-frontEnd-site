cce0982a159c7542ea10e4203dac8e4c6464a39a
Akin Aguda <lordtitan7@gmail.com>
2019-04-06 19:25:00 +0100

    removed build from gitignore

---
+++ build/precache-manifest.005e8778a0c915792d7a5bb2e6102176.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "50e047d0232ef5502afb071fa5ae7698",
+    "url": "/ecx-frontEnd-site/static/media/brand.50e047d0.svg"
+  },
+  {
+    "revision": "364408b68e7b3a01d557",
+    "url": "/ecx-frontEnd-site/static/js/runtime~main.5231336b.js"
+  },
+  {
+    "revision": "60855d032edd87be4837",
+    "url": "/ecx-frontEnd-site/static/js/main.1fcf0397.chunk.js"
+  },
+  {
+    "revision": "51b4e4a507214ea45a1d",
+    "url": "/ecx-frontEnd-site/static/js/2.be5d2d91.chunk.js"
+  },
+  {
+    "revision": "60855d032edd87be4837",
+    "url": "/ecx-frontEnd-site/static/css/main.c24f3013.chunk.css"
+  },
+  {
+    "revision": "991440f621a900fa22a2517f789beacd",
+    "url": "/ecx-frontEnd-site/index.html"
+  }
+];
